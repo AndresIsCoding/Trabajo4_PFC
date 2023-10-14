ac2d6a922bf086744a2fb603c6f8c887b3e891c4
Andres <asprilla.andres@correounivalle.edu.co>
2023-10-13 23:10:49 -0500

    Primer Push

---
+++ src/main/scala/SubsecuenciaMasLarga/package.scala
@@ -0,0 +1,40 @@
+package object SubsecuenciaMasLarga
+{
+  type Secuencia = Seq[Int]
+  type Subsecuencia = Seq[Int]
+
+
+  def subSecuenciaAsoc(s:Secuencia, inds: Seq[Int]): Subsecuencia =
+  {
+
+  }
+  def subSecuenciasDe(s:Secuencia): Set[Subsecuencia] =
+  {
+
+  }
+
+  def incremental(s: Secuencia): Boolean =
+  {
+
+  }
+
+  def subSecuenciasInc(s:Secuencia): Set[Subsecuencia] =
+  {
+
+  }
+
+  def subsecuenciaIncrementalMasLarga(s: Secuencia): Subsecuencia =
+  {
+
+  }
+
+  def ssimIComenzandoEn(i:Int , s: Secuencia): Subsecuencia =
+  {
+
+  }
+
+  def subSecIncMasLargaV2(s: Secuencia): Subsecuencia =
+  {
+
+  }
+}
