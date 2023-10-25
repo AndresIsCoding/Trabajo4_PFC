c2e718716391e49f333a3392a4583a047779705e
judajaavAlt <132619919+judajaavAlt@users.noreply.github.com>
2023-10-25 16:55:33 -0500

    fuerza bruta (aun se pueden mejorar en cirtas formas)

--- src/main/scala/SubsecuenciaMasLarga/package.scala
+++ src/main/scala/SubsecuenciaMasLarga/package.scala
@@ -1,29 +1,37 @@
+import scala.collection.immutable.{Seq, Set}
+
 package object SubsecuenciaMasLarga
 {
   type Secuencia = Seq[Int]
   type Subsecuencia = Seq[Int]
+
   def subindices(i:Int, n:Int): Set[Seq[Int]] =
   {
-    val initial = (for
-  {
-    number <- i until n
-  } yield Seq(number)) ++ Seq()
-
-    def sequencia(l1:Set[Seq[Int]], l2:Set[Seq[Int]], max_size:Int): Set[Seq[Int]] =
+    //
+    def sequencia(l1: Set[Seq[Int]], l2: Set[Seq[Int]], max_size: Int): Set[Seq[Int]] =
+    {
+      val nueva_sequencia = for
       {
-        val nueva_sequencia = for
-        {
-          numbers_1 <- l1
-          numbers_2 <- l2
-          if (numbers_1.last < numbers_2.head)
-        } yield numbers_1 ++ numbers_2
-        if (l1.head.size == max_size) Set(Seq())
-        else nueva_sequencia ++ sequencia(nueva_sequencia, l1, max_size)
+        numbers_1 <- l1
+        numbers_2 <- l2
+        if (numbers_1.last < numbers_2.head)
+      } yield numbers_1 ++ numbers_2
 
-      }
-
-    initial.toSet ++ sequencia(initial.toSet, initial.toSet, n - i)
+      if (l1.head.size == max_size) Set(Seq())
+      else nueva_sequencia ++ sequencia(nueva_sequencia, l2, max_size)
     }
+
+    if (i < n)
+    {
+      val initial = (for {number <- i until n} yield Seq(number)) ++ Seq()
+      initial.toSet ++ sequencia(initial.toSet, initial.toSet, n - i)
+    }else if (i == n) Set(Seq())
+    else
+      {
+        println("i debe ser menor o igual a n")
+        Set(Seq())
+      }
+  }
 
   def subSecuenciaAsoc(s:Secuencia, inds: Seq[Int]): Subsecuencia =
   {
@@ -38,52 +46,63 @@ package object SubsecuenciaMasLarga
 
   def subSecuenciasDe(s:Secuencia): Set[Subsecuencia] =
   {
-    val secuencias_iniciales = for {
-      valor <- s
-    } yield Seq(valor)
-
-    def sequencia(l1: Set[Seq[Int]], l2: Set[Seq[Int]], max_size: Int): Set[Seq[Int]] =
+    for
     {
-      val nueva_sequencia = for
+      indice <- subindices(0, s.size)
+    }yield subSecuenciaAsoc(s,indice)
+  }
+
+  def incremental(s: Secuencia): Boolean =
       {
-        numbers_1 <- l1
-        numbers_2 <- l2
-        if(numbers_1.last < numbers_2.head)
-      } yield numbers_1 ++ numbers_2
-      if (l1.head.size == max_size) Set(Seq())
-      else nueva_sequencia ++ sequencia(nueva_sequencia, l1, max_size)
-    }
-
-    secuencias_iniciales.toSet ++ sequencia(secuencias_iniciales.toSet,secuencias_iniciales.toSet, s.size)
-}
-
-
-      def incremental(s: Secuencia): Boolean =
-      {
-        val pairs = (for{value <- s.init} yield value) zip (for{value <- s.tail} yield value)
-
-        val comparation = for
+        if(s.size > 1)
         {
+          val pairs = (for {value <- s.init} yield value) zip (for {value <- s.tail} yield value)
+
+        val comparation = for {
           pair <- pairs
 
-          if(pair._1 < pair._2)
-        }yield 1
+          if (pair._1 < pair._2)
+        } yield 1
 
         if (comparation.size == pairs.size) true
         else false
+        }else true
+
       }
 
+  def subSecuenciasInc(s:Secuencia): Set[Subsecuencia] =
+      {
+        val subsecuencias = subSecuenciasDe(s)
+
+        for
+        {
+          subsecuencia <- subsecuencias
+          if(incremental(subsecuencia))
+        }yield subsecuencia
+      }
+
+
+  def subsecuenciaIncrementalMasLarga(s: Secuencia): Subsecuencia =
+  {
+    val subsecuencias_incrementales = subSecuenciasInc(s)
+
+    def secuencia_mayor(s: Set[Subsecuencia], c: Int): Subsecuencia =
+    {
+      val subsecuencias_mayores =
+      for
+      {
+        secuencia <- s
+        if (secuencia.size > c)
+      } yield secuencia
+
+      if(subsecuencias_mayores.size == 0) s.head
+      else secuencia_mayor(subsecuencias_mayores, c+1)
+    }
+
+    secuencia_mayor(subsecuencias_incrementales, 0)
+
+  }
   /*
-        def subSecuenciasInc(s:Secuencia): Set[Subsecuencia] =
-        {
-
-        }
-
-        def subsecuenciaIncrementalMasLarga(s: Secuencia): Subsecuencia =
-        {
-
-        }
-
         def ssimIComenzandoEn(i:Int , s: Secuencia): Subsecuencia =
         {
 
